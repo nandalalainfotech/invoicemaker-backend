010569b0216d4e3243681350281df5df2b7a1df9
moorthy <moorthy.p@primesoftinc.com>
2023-11-14 11:28:22 +0530

    Backend Changes done by thiru..

--- Routers/invoiceRouter.js
+++ Routers/invoiceRouter.js
@@ -5,17 +5,17 @@ import invoiceModel from "../Models/invoiceModel.js";
 
 const invoiceRouter = express.Router();
 
-invoiceRouter.post("/", async (request, response) => {
+invoiceRouter.post("/invoicedetail", async (request, response) => {
 
-    console.log("req----->", request);
-    const invoice = new invoiceModel(request.body);
-  
-    try {
-      await invoice.save();
-      response.send(invoice);
-    } catch (error) {
-      response.status(500).send(error);
-    }
+  // console.log("req----->", request);
+  const invoice = new invoiceModel(request.body);
+
+  try {
+    await invoice.save();
+    response.send(invoice);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 export default invoiceRouter;
