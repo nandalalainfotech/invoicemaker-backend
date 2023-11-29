5405d28bf692756fae6058fe10d90f77c7a5394c
suriyavjn <suriyavjn@gmail.com>
2023-11-29 17:03:30 +0530

    backend invoiceRouter changes done by Naresh.

--- Models/invoiceModel.js
+++ Models/invoiceModel.js
@@ -1,21 +1,27 @@
 import mongoose from "mongoose";
 
 const InvoiceSchema = new mongoose.Schema({
-    companyName: { type: String, required: false },
-    billingAddress: { type: String, required: false },
-    companyEmail: { type: String, required: false },
-    companyMobile: { type: String, required: false },
     clientName: { type: String, required: false },
     clientAddress: { type: String, required: false },
     clientEmail: { type: String, required: false },
-    clientMobileNo: { type: String, required: false },
+    clientNo: { type: String, required: false },
     invoiceNo: { type: String, required: false },
+    changeCurrency: { type: String, required: false },
     createdDate: { type: String, required: false },
-    dueDate: { type: String, required: false },
-    products: [],
-    subTotal: { type: String, required: false },
+    Duedate: { type: String, required: false },
+    test: [],
+    Tax: { type: String, required: false },
+    Discount: { type: String, required: false },
+    shipping: { type: String, required: false },
+    Balance: { type: String, required: false },
+    Amount: { type: String, required: false },
+    Total: { type: String, required: false },
+    subtotal: { type: String, required: false },
+    Email: { type: String, required: false },
+    MobileNo: { type: String, required: false },
+    Company: { type: String, required: false },
+    CompanyName: { type: String, required: false },
     userid: { type: String, required: false },
-
 });
 
 const Invoice = mongoose.model("invoice", InvoiceSchema);
--- Routers/invoiceRouter.js
+++ Routers/invoiceRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
-import invoiceModel from "../Models/invoiceModel.js";
-import Invoice from "../Models/invoiceModel.js";
 import expressAsyncHandler from "express-async-handler";
-import pdf from "dynamic-html-pdf";
 import fs, { createReadStream } from "fs";
+import Invoice from "../Models/invoiceModel.js";
+import pdf from "dynamic-html-pdf";
+
 
 const invoiceRouter = express.Router();
 
 invoiceRouter.post("/invoicedetail", async (request, response) => {
-
-  const invoice = new invoiceModel(request.body);
+  const invoice = new Invoice(request.body);
 
   try {
     await invoice.save();
@@ -31,17 +30,38 @@ invoiceRouter.get(
     }
   })
 );
-
-invoiceRouter.delete('/deleteInvoice/:id', expressAsyncHandler(async (req, res) => {
-  const productId = req.params.id;
-  const invoice = await Invoice.findById(productId);
-  if (invoice) {
-    const deletenewInvoice = await invoice.deleteOne();
-    res.send({ message: "Attributed Deleted", deleteAtt: deletenewInvoice });
+invoiceRouter.put("/updateInvoice/:id", expressAsyncHandler(async (req, res) => {
+  const invoiceUpdateId = req.params.id;
+  const invoiceupdate = await Invoice.findById(invoiceUpdateId);
+  if (invoiceupdate) {
+    invoiceupdate.clientName = req.body.clientName;
+    invoiceupdate.clientAddress = req.body.clientAddress;
+    invoiceupdate.clientEmail = req.body.clientEmail;
+    invoiceupdate.clientNo = req.body.clientNo;
+    invoiceupdate.invoiceNo = req.body.invoiceNo;
+    invoiceupdate.changeCurrency = req.body.changeCurrency;
+    invoiceupdate.createdDate = req.body.createdDate;
+    invoiceupdate.Duedate = req.body.Duedate;
+    invoiceupdate.test = req.body.test;
+    invoiceupdate.Tax = req.body.Tax;
+    invoiceupdate.Discount = req.body.Discount;
+    invoiceupdate.shipping = req.body.shipping;
+    invoiceupdate.Balance = req.body.Balance;
+    invoiceupdate.Amount = req.body.Amount;
+    invoiceupdate.Total = req.body.Total;
+    invoiceupdate.subtotal = req.body.subtotal;
+    invoiceupdate.Email = req.body.Email;
+    invoiceupdate.MobileNo = req.body.MobileNo;
+    invoiceupdate.Company = req.body.Company;
+    invoiceupdate.CompanyName = req.body.CompanyName;
+    const updateinvoice = await invoiceupdate.save();
+    res.send({ message: "Updated", orderdetail: updateinvoice });
   } else {
-    res.status(404).send({ message: "Product Not Found" });
+    res.status(404).send({ message: "Orderdetail Detail Not Found" });
   }
-}));
+})
+);
+
 
 invoiceRouter.get(
   "/downloaduser/:id",
@@ -97,38 +117,32 @@ invoiceRouter.get(
   })
 );
 
+
 invoiceRouter.get(
-  "/downloadALLPDF",
-  expressAsyncHandler(async (req, response) => {
+  '/downloadALLPDF', expressAsyncHandler(async (req, res) => {
+
     let datas = [];
     var usersDetails = await Invoice.find();
     var lastIndex = usersDetails.length - 1;
     var lastObject = usersDetails[lastIndex];
     datas.push(lastObject);
-
     var html = fs.readFileSync(`pdf.html`, "utf8");
     var options = {
       format: "A3",
       orientation: "portrait",
       border: "10mm",
     };
-
     let data = lastObject;
-    const products = data?.products;
-  
     var document = {
       type: "file", // 'file' or 'buffer'
-      target :"blank",
+      target: "blank",
       template: html,
       context: {
         invoice: data,
-        invoiceProducts: products,
+        invoiceProducts: data.test,
       },
       path: "./output.pdf", // it is not required if type is buffer
     };
-
-
-
     if (data?.length === 0) {
       return null;
     }
@@ -137,18 +151,41 @@ invoiceRouter.get(
         .create(document, options)
         .then((pathRes) => {
           const filestream = createReadStream(pathRes.filename);
-          response.writeHead(200, {
+          res.writeHead(200, {
             "Content-Disposition": "attachment;filename=" + "purchasSlips.pdf",
             "Content-Type": "application/pdf",
           });
-          filestream.pipe(response);
+          filestream.pipe(res);
         })
         .catch((error) => {
           console.error(error);
         });
     }
   })
-);
+)
 
+
+
+invoiceRouter.delete('/deleteInvoice/:id', expressAsyncHandler(async (req, res) => {
+  const productId = req.params.id;
+  const invoice = await Invoice.findById(productId);
+  if (invoice) {
+    const deletenewInvoice = await invoice.deleteOne();
+    res.send({ message: "Attributed Deleted", deleteAtt: deletenewInvoice });
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+}));
+
+invoiceRouter.get("/editInvoice/:id", expressAsyncHandler(async (req, res) => {
+  const invoiceDetailId = req.params.id;
+  const invoiceId = await Invoice.findById({ _id: invoiceDetailId });
+  if (invoiceId) {
+    res.send(invoiceId);
+  } else {
+    res.status(404).send({ message: "Invoice Detail Not Found" });
+  }
+})
+);
 
 export default invoiceRouter;
